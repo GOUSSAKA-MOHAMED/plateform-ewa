52bbeb21862272ab7a8ee433f44c03ae0fcf688f
goussaka-mohamed <goussakamohamed123@gmail.com>
2022-05-31 12:00:41 +0100

    goussaka

---
+++ main.js
@@ -0,0 +1,15 @@
+let title_content = "elzero"
+     desc_content = "elzero web school"
+     date_content = "25/10";
+
+let card_cont = `
+     <div class="contains">
+          <div class="content_card">
+               <h3> title </h3>
+               <p> paragraph </p>
+               <span>  </span>
+          </div>
+     </div>
+`;
+document.card_cont.repeat(4);
+document.write(card_cont);
